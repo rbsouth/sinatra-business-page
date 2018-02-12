554f705034bb4aad642e7157beff03d6aeb2b320
Reid South <rbsouth@gmail.com>
2018-02-12 12:53:04 -0700

    email works. images added, pages added

--- main.rb
+++ main.rb
@@ -1,5 +1,25 @@
 require 'sinatra'
+require 'pony'
 
 get '/' do
 	erb :home	
+end
+
+get '/about' do
+	erb :about	
+end
+
+get '/gallery' do
+	erb :gallery	
+end
+
+get '/contact' do
+	erb :contact	
+end
+
+post '/contact' do
+	  Pony.mail :to => 'rbsouth@gmail.com',
+              :from => params[:email],
+              :subject => params[:title],
+              :body =>  params[:email] +" wrote:\n" + params[:message]
 end
